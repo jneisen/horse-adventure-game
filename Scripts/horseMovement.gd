08f518b0182ea5ec608e4fa2aa18315cb7f3486e
jneisen <joseph@neisen.xyz>
2025-02-07 22:25:00 -0500

    Ladder done, but now I need to do screens/scroll properly

--- Scripts/horseMovement.gd
+++ Scripts/horseMovement.gd
@@ -2,8 +2,9 @@ extends CharacterBody2D
 
 @export var speed = 400
 @export var sprite : AnimatedSprite2D
+@onready var ladder : Node2D = $"/root/Main/Horse/Ladder"
 var heldItem = "ladder"
-var right
+var right #bool
 
 func _process(delta):
 	velocity = Vector2.ZERO
@@ -35,14 +36,22 @@ func _process(delta):
 func _physics_process(delta: float) -> void:
 	move_and_slide()
 
+func create_ladder(y_pos):
+	#rather than actually creating a ladder, lets just store the
+	#ladder and move it close
+	ladder.global_position = Vector2(global_position.x, y_pos) + Vector2(0, -50)
+func hide_ladder():
+	ladder.position = Vector2(1500, 1500)
+	
+
 func _collide_with_cliff(collider):
 	#if we collide and our held item is the ladder,
 	#spawn ladder object above us
 	if(heldItem == "ladder"):
-		print("spawn ladder")
+		create_ladder(collider.global_position.y)
 		collider.get_node("CollisionShape2D").set_deferred("disabled", true)
 		
 func _leave_cliff(collider):
 	#despawn the ladder object
-	print("despawn ladder")
+	hide_ladder()
 	collider.get_node("CollisionShape2D").set_deferred("disabled", false)
